4bb9673402a11147de748c2f94197a22d53991f4
jpingithub <nallabothulanagarjun0@gmail.com>
2025-03-18 11:08:11 +0530

    Made it working in local

--- src/main/java/com/rb/auth/client/UserManagementClient.java
+++ src/main/java/com/rb/auth/client/UserManagementClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "User-Management-Service", url = "${user-management-service-url}")
+@FeignClient(name = "USER-MANAGEMENT-SERVICE")
 public interface UserManagementClient {
 
     @GetMapping("/api/v1/users/login")
